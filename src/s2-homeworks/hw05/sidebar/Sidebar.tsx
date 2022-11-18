ba937ad1680fe91f6ed76247a01ab59ba8bb9643
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2022-11-18 12:00:15 -0500

    some changes done

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -17,7 +17,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
 
-            {open && <aside className={sidebarClass}>
+            <aside className={sidebarClass}>
                 <button className={s.close} onClick={handleClose}>
                     <img
                         src={closeIcon}
@@ -54,7 +54,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         Junior Plus
                     </NavLink>
                 </nav>
-            </aside>}
+            </aside>
         </>
     )
 }
